2a2c35175a4051a37ef15a5dc5b021d62dd5c67d
masahiro-0420 <masahiro.kisogawa@icloud.com>
2021-03-14 19:30:16 +0900

    logout modification commit

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,5 +1,5 @@
 class PrototypesController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, only: [:new, :edit, :destroy]
 
   def index
     @prototypes = Prototype.all
